fa21fc3e15dec999b7377e61909bddb41a332874
Kate Farkas <kate.m.farkas@gmail.com>
2020-03-31 14:24:36 -0700

    add first testing parameter, change jq version so it actually works >_<

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,15 @@
+// bs logic
+var leapYear = function(year) {
+  return false;
+};
+
+// ui logic
 $(document).ready(function() {
   $("form#leap-year").submit(function(event) {
     event.preventDefault();
     var year = parseInt($("input#year").val());
+    
+
     var result = leapYear(year);
     $("#result").text(result);
   });
