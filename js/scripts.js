c0ab7fecf445ef7ae7a240cb9891d2345eed7abd
Kate Farkas <kate.m.farkas@gmail.com>
2020-03-31 14:50:04 -0700

    add friendly div text, show/hide, and insert result (or not) with jq

--- js/scripts.js
+++ js/scripts.js
@@ -12,9 +12,16 @@ $(document).ready(function() {
   $("form#leap-year").submit(function(event) {
     event.preventDefault();
     var year = parseInt($("input#year").val());
-    
-
     var result = leapYear(year);
-    $("#result").text(result);
+
+    $(".year").text(year);
+
+    if (!result) {
+      $(".not").text("not");
+    } else {
+      $(".not").text("");
+    }
+
+    $("#result").show();
   });
 });
