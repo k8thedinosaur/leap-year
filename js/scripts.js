dbc024928f5965d473e4d64654d87fcb08baa162
Kate Farkas <kate.m.farkas@gmail.com>
2020-03-31 14:36:26 -0700

    add fourth pampamparam, refactor code

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,6 @@
 // bs logic
 var leapYear = function(year) {
-  if (year % 100 ===0) {
-    return false;
-  } else if (year % 4 === 0) {
+  if ((year % 100 === 0) && (year % 4 === 0 ) || (year % 400 === 0)) {
     return true;
   } else {
   return false;
